da374c4e060187a12f29020cb397f2715fe808c0
horo27softwire <valentin.hororoi@softwire.com>
2026-07-09 12:33:44 +0300

    addead decrease SellIn function

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -39,6 +39,12 @@ export class GildedRose {
         }
     }
 
+    decreaseSellIn(item: Item) {
+        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -46,9 +52,9 @@ export class GildedRose {
             } else {
                 this.increaseQuality(this.items[i]);
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
+
+            this.decreaseSellIn(this.items[i]);
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
