bd96da94673cf35aa1bfb1efa9e1cfe244ef6ed2
horo27softwire <valentin.hororoi@softwire.com>
2026-07-09 14:33:57 +0300

    added notSpecialItem function

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -43,12 +43,16 @@ export class GildedRose {
         item.sellIn = item.sellIn - 1;
     }
 
+    notSpecialItem(item: Item) {
+        return item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert';
+    }
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
 
             if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') continue;
 
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.notSpecialItem(this.items[i])) {
                 this.decreaseQuality(this.items[i]);
             } else {
                 this.increaseQuality(this.items[i]);
@@ -57,7 +61,7 @@ export class GildedRose {
             this.decreaseSellIn(this.items[i]);
 
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.notSpecialItem(this.items[i])) {
                     this.decreaseQuality(this.items[i]);
                 } else {
                     if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
