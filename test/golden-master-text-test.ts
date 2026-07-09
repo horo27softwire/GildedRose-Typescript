8dfab878345f421a51b38c155df756af3cc078b4
horo27softwire <valentin.hororoi@softwire.com>
2026-07-09 12:05:11 +0300

    added master test

--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,3 +1,44 @@
 import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
 
 // Add a master test here
+
+describe('Gilded Rose', function () {
+
+    it('MT 1', function() {
+        const gildedRose = new GildedRose([
+            new Item('Sulfuras, Hand of Ragnaros', 100, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 20, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 6, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 3, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 1, 100),
+            new Item('Aged Brie', 1, 45),
+            new Item('Aged Brie', 1, 50),
+            new Item('Magic Crow', 14, 30),
+            new Item('Magic Crow', 14, 1),
+            new Item('Cow Powder', 0, 25),
+            new Item('Conjured Tomato', 7, 10),
+            new Item('Conjured Ketchup', 1, 10)
+        ]);
+
+        const items = new GildedRose(gildedRose.updateQuality()).updateQuality();
+
+        expect(items).to.equal([
+            new Item('Sulfuras, Hand of Ragnaros', 100, 80),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 18, 22),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 7, 24),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 4, 25),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 1, 26),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0),
+            new Item('Aged Brie', 0, 48),
+            new Item('Aged Brie', 0, 50),
+            new Item('Magic Crow', 12, 28),
+            new Item('Magic Crow', 12, 0),
+            new Item('Cow Powder', 0, 21),
+            new Item('Conjured Tomato', 5, 6),
+            new Item('Conjured Ketchup', 0, 4)
+        ]);
+    });
+
+});
